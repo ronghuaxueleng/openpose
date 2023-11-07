b3724c0646b51fa9f65779a721120c2c9a5b29cc
caoqiang <caoqianghappy@126.com>
2023-11-07 22:57:49 +0800

    up

--- mix_face_to_body.py
+++ mix_face_to_body.py
@@ -31,15 +31,12 @@ if __name__ == '__main__':
         fix_full_body_path = os.path.join(root_path, json_dir, 'full_body_fix.json')
         face, face_json = getOneSpecificData(face_path)
         body, body_json = getOneSpecificData(full_body_path)
+        body_json['people'][0]['pose_keypoints_2d'] = []
         for index, row in face.iterrows():
             if row['X'] != 0.0 and row['Y'] != 0.0 and row['P'] != 0.0:
-                body['X'][index] = row['X']
-                body['Y'][index] = row['Y']
-        body_json['people'][0]['pose_keypoints_2d'] = []
-        for index, row in body.iterrows():
-            body_json['people'][0]['pose_keypoints_2d'].append(row['X'])
-            body_json['people'][0]['pose_keypoints_2d'].append(row['Y'])
-            body_json['people'][0]['pose_keypoints_2d'].append(row['P'])
+                body_json['people'][0]['pose_keypoints_2d'].append(row['X'])
+                body_json['people'][0]['pose_keypoints_2d'].append(row['Y'])
+                body_json['people'][0]['pose_keypoints_2d'].append(row['P'])
         if 'face_keypoints_2d' in face_json['people'][0]:
             body_json['people'][0]['face_keypoints_2d'] = face_json['people'][0]['face_keypoints_2d']
         with open(fix_full_body_path, "w") as sf:
