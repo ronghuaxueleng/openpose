67c138a0005a26e67bc18c825cea8bba3ede66e2
caoqiang <caoqiang>
2023-11-07 11:44:59 +0800

    up

--- main.py
+++ main.py
@@ -4,6 +4,34 @@ import os
 import cv2
 import numpy as np
 
+
+# {0,  "Nose"},
+# {1,  "Neck"},
+# {2,  "RShoulder"},
+# {3,  "RElbow"},
+# {4,  "RWrist"},
+# {5,  "LShoulder"},
+# {6,  "LElbow"},
+# {7,  "LWrist"},
+# {8,  "MidHip"},
+# {9,  "RHip"},
+# {10, "RKnee"},
+# {11, "RAnkle"},
+# {12, "LHip"},
+# {13, "LKnee"},
+# {14, "LAnkle"},
+# {15, "REye"},
+# {16, "LEye"},
+# {17, "REar"},
+# {18, "LEar"},
+# {19, "LBigToe"},
+# {20, "LSmallToe"},
+# {21, "LHeel"},
+# {22, "RBigToe"},
+# {23, "RSmallToe"},
+# {24, "RHeel"},
+# {25, "Background"}
+
 # 骨骼关键点连接对
 pose_pairs = [
     [0, 1], [0, 15], [0, 16],
