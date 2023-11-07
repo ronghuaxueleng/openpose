0a5128269fe24d62c9948efbffbb2c80ca5357bb
caoqiang <caoqiang>
2023-11-07 17:22:29 +0800

    up

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ def getOneSpecificData(json_path):
         data = json.load(f)
         data = np.array(data['people'][0]['pose_keypoints_2d']).reshape(-1, 3)
         return pd.DataFrame(data, columns=['X', 'Y', 'P'],
-                          index=idnex_map[len(data)])
+                            index=idnex_map[len(data)])
 
 
 if __name__ == '__main__':
@@ -28,10 +28,18 @@ if __name__ == '__main__':
     for json_dir in json_dir_list:
         face_path = os.path.join(root_path, json_dir, 'face.json')
         full_body_path = os.path.join(root_path, json_dir, 'full_body.json')
+        fix_full_body_path = os.path.join(root_path, json_dir, 'full_body_fix.json')
         face = getOneSpecificData(face_path)
         body = getOneSpecificData(full_body_path)
         for index, row in face.iterrows():
             if row['X'] != 0.0 and row['Y'] != 0.0 and row['P'] != 0.0:
                 body['X'][index] = row['X']
                 body['Y'][index] = row['Y']
-        print()
+        with open(full_body_path) as f:
+            data = json.load(f)
+            for index, row in body.iterrows():
+                data['people'][0]['pose_keypoints_2d'].append(row['X'])
+                data['people'][0]['pose_keypoints_2d'].append(row['Y'])
+                data['people'][0]['pose_keypoints_2d'].append(row['P'])
+            with open(fix_full_body_path, "w") as sf:
+                json.dump(data, sf)
